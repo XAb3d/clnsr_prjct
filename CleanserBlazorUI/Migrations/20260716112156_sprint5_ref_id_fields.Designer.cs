8368f5a09483e6165eb05e6b8b747e2abbbb9c50
Abednego Addy <AAddy@xdsdatagh.com>
2026-07-16 11:47:20 +0000

    ref_id_field

--- CleanserBlazorUI/Migrations/20260716112156_sprint5_ref_id_fields.Designer.cs
+++ CleanserBlazorUI/Migrations/20260716112156_sprint5_ref_id_fields.Designer.cs
@@ -4,6 +4,7 @@ using CleanserBlazorUI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanserBlazorUI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260716112156_sprint5_ref_id_fields")]
+    partial class sprint5_ref_id_fields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CleanserBlazorUI/Migrations/20260716112156_sprint5_ref_id_fields.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanserBlazorUI.Migrations
+{
+    /// <inheritdoc />
+    public partial class sprint5_ref_id_fields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DriverLicNum",
+                table: "IndividualsData",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EzwichNum",
+                table: "IndividualsData",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdatedDate",
+                table: "IndividualsData",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "NatIDNum",
+                table: "IndividualsData",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherIDNum",
+                table: "IndividualsData",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PassportNum",
+                table: "IndividualsData",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SSNum",
+                table: "IndividualsData",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VotersIDNum",
+                table: "IndividualsData",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdatedDate",
+                table: "BusinessesData",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DriverLicNum",
+                table: "IndividualsData");
+
+            migrationBuilder.DropColumn(
+                name: "EzwichNum",
+                table: "IndividualsData");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedDate",
+                table: "IndividualsData");
+
+            migrationBuilder.DropColumn(
+                name: "NatIDNum",
+                table: "IndividualsData");
+
+            migrationBuilder.DropColumn(
+                name: "OtherIDNum",
+                table: "IndividualsData");
+
+            migrationBuilder.DropColumn(
+                name: "PassportNum",
+                table: "IndividualsData");
+
+            migrationBuilder.DropColumn(
+                name: "SSNum",
+                table: "IndividualsData");
+
+            migrationBuilder.DropColumn(
+                name: "VotersIDNum",
+                table: "IndividualsData");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedDate",
+                table: "BusinessesData");
+        }
+    }
+}
