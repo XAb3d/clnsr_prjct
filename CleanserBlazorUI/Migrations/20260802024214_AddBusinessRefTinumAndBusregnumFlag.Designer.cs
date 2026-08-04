96dec4b301e2f5c1ec8f7c96f09f28937f50bc4a
XAb3d <aaddyxds@gmail.com>
2026-08-04 08:10:07 +0000

    migrations reapplied, Add missing migrations: SubscriberProfile, IndividualRef.MiddleNames, FacilityStatusCode, BusinessRef Tinum/Busregnum flag

--- CleanserBlazorUI/Migrations/20260802024214_AddBusinessRefTinumAndBusregnumFlag.Designer.cs
+++ CleanserBlazorUI/Migrations/20260802024214_AddBusinessRefTinumAndBusregnumFlag.Designer.cs
@@ -4,6 +4,7 @@ using CleanserBlazorUI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanserBlazorUI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260802024214_AddBusinessRefTinumAndBusregnumFlag")]
+    partial class AddBusinessRefTinumAndBusregnumFlag
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CleanserBlazorUI/Migrations/20260802024214_AddBusinessRefTinumAndBusregnumFlag.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanserBlazorUI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBusinessRefTinumAndBusregnumFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tinum",
+                table: "BusinessesData",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tinum",
+                table: "BusinessesData");
+        }
+    }
+}
